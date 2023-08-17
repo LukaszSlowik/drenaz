5858b86a94bf29c47eefd3190ae38de8d04ee01f
Lukasz <lukasz.slowik.osw@gmail.com>
2023-08-17 09:30:58 +0200

    first commit

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -1,10 +1,11 @@
 "use client";
+import { cn } from "@/lib/utils";
 import React, { useEffect, useState } from "react";
 
 type Props = {};
 
 let indexOfExcercise = 0;
-const typeOfExcercises = ["PEP na 2.0", "PEP na 2.5", "Aerobika"];
+const typeOfExcercises = ["PEP 2.0", "PEP 2.5", "Aerobika"];
 const series = [1, 2, 3];
 const Counter = (props: Props) => {
   const [count, setCount] = React.useState(10);
@@ -13,11 +14,13 @@ const Counter = (props: Props) => {
   );
   const [serie, setSerie] = React.useState<number>(series[0]);
   const [endOfSeries, setEndOfSeries] = React.useState(false);
+  const [rotate, setRotate] = React.useState(0);
 
   console.log(typeOfExcercise);
   const [disabled, setDisabled] = React.useState(false);
 
   const handleCounterClick = () => {
+    setRotate(1);
     let newCount = count === 1 ? 10 : count - 1;
     if (count === 1) {
       if (serie === 3 && indexOfExcercise === 2) {
@@ -58,13 +61,24 @@ const Counter = (props: Props) => {
       <div className="flex   flex-1 w-full h-full relative">
         <button
           onClick={handleCounterClick}
+          onAnimationEnd={() => setRotate(0)}
           disabled={disabled}
-          className="rounded-[50%] bg-green-500 aspect-square w-2/4 sm:w-72  absolute top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2 transform text-white text-8xl flex items-center justify-center"
+          data-clicked={rotate}
+          className={cn(
+            "cursor-pointer rounded-[50%] bg-green-500 aspect-square w-2/4 sm:w-72 transform-  absolute top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2  text-white text-8xl flex items-center justify-center"
+          )}
         >
           {endOfSeries ? (
             <span className="text-4xl text-center">Koniec {`:)`}</span>
           ) : (
-            count
+            <span
+              className={cn()
+              // rotate === 1 && "animate-[rotate-360_1s_ease-in-out]"
+              }
+            >
+              {" "}
+              {count}
+            </span>
           )}
         </button>
       </div>
